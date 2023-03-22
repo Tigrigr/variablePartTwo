4368294afb52dc7ae36290f392fe63d216c38b7c
mikha <Mikhailov.dzenphone@gmail.com>
2023-03-22 12:45:13 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-
+        task2();
     }
 
     public static void task1 () {
+        System.out.println("Задача 1");
         byte b = 120;
         short s = 32544;
         int i = 212345678;
@@ -18,5 +19,22 @@ public class Main {
         System.out.println("Значение переменной f с типом float равно " + f);
         System.out.println("Значение переменной d с типом double равно " + d);
     }
-
+    public static void task2 () {
+        System.out.println();
+        System.out.println("Задача 2");
+        float f = 27.12f;
+        long l = 987678965549L;
+        float f2 = 2.786f;
+        short s = 569;
+        short s2 = -159;
+        short s3 = 27897;
+        byte b = 67;
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(f2);
+        System.out.println(s);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(b);
+    }
 }
